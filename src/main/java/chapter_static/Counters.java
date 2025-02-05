89cba8e9e610025dbde9890096b94c8fe9189a57
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-05 15:30:37 +0100

    create package chapter_static in java and test, create classes Counters and CountersTest, tdd created method Counters.incrementTotalCount. All fits.

---
+++ src/main/java/chapter_static/Counters.java
@@ -0,0 +1,51 @@
+package chapter_static;
+
+import java.util.Objects;
+
+public class Counters {
+    protected static int totalCount = 0;
+
+    protected int instanceCount = 0;
+
+    public Counters() {}
+
+    public Counters(int instanceCount) {
+        this.instanceCount = instanceCount;
+    }
+
+    public static int getTotalCount() {
+        return totalCount;
+    }
+
+    public static void setTotalCount(int totalCount) {
+        Counters.totalCount = totalCount;
+    }
+
+    public static int incrementTotalCount() {
+        totalCount++;
+
+//        if (totalCount == 4) // for verifying test incrementTotalCountExpectedIncreasedBy1EveryTimeWhenCalled
+//            return 42;
+
+        return  totalCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Counters counters = (Counters) o;
+        return instanceCount == counters.instanceCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(instanceCount);
+    }
+
+    @Override
+    public String toString() {
+        return "Counters{" +
+                "instanceCount=" + instanceCount +
+                '}';
+    }
+}
---
+++ src/test/java/chapter_static/CountersTest.java
@@ -0,0 +1,45 @@
+package chapter_static;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountersTest {
+
+    @BeforeEach
+    public void cleanAll() {
+        Counters.setTotalCount(0);
+    }
+
+    @Test
+    public void incrementTotalCountExpected1WhenCalled() {
+        // given
+        // Counters as class without instance
+        int expected = 1;
+
+        // when
+        int actual = Counters.incrementTotalCount();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void incrementTotalCountExpectedIncreasedBy1EveryTimeWhenCalled() {
+        // given
+        // Counters as class without instance
+        int expected = 1;
+
+        // when
+        for (int i = 0; i < 10; i++) {
+            int actual = Counters.incrementTotalCount();
+
+            // then
+            assertEquals(expected, actual);
+
+            // for next loop pass
+            expected++;;
+        }
+    }
+}
