176a80c02d6dd2639d56cb164f6b7ce686dac9df
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-05 11:24:27 +0100

    declare and implement test methods equalsExpectedTrueWhenCompare2CarsIdenticallyInitialized and equalsExpectedFalseWhenCompare2CarsIdenticallyButNumberOfDoorsInitialized in CarTest

--- src/test/java/inheritance/CarTest.java
+++ src/test/java/inheritance/CarTest.java
@@ -1,4 +1,36 @@
 package inheritance;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CarTest {
+
+    @Test
+    public void equalsExpectedTrueWhenCompare2CarsIdenticallyInitialized() {
+        // given
+        Car car1 = new Car("Opel", "Meriva A", 2009, 5);
+        Car car2 = new Car("Opel", "Meriva A", 2009, 5);
+        boolean expected = true;
+
+        // when
+        boolean actual = car1.equals(car2);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void equalsExpectedFalseWhenCompare2CarsIdenticallyButNumberOfDoorsInitialized() {
+        // given
+        Car car1 = new Car("Opel", "Meriva A", 2009, 5);
+        Car car2 = new Car("Opel", "Meriva A", 2009, 3);
+        boolean expected = false;
+
+        // when
+        boolean actual = car1.equals(car2);
+
+        // then
+        assertEquals(expected, actual);
+    }
 }
