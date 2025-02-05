68b48d14794a074cddb9a4d1379b43a9684b121f
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-05 16:39:43 +0100

    call all 4 methods in main

--- src/main/java/Main_chapter_static.java
+++ src/main/java/Main_chapter_static.java
@@ -1,3 +1,4 @@
+import chapter_static.Calculator;
 import chapter_static.Counters;
 
 public class Main_chapter_static {
@@ -25,5 +26,14 @@ public class Main_chapter_static {
         System.out.println(myCounters1);
         System.out.println(myCounters2);
         System.out.println(myCounters3);
+
+        System.out.println();
+        System.out.println("Calculator operations");
+        System.out.println();
+
+        System.out.println("33 + 9 = " + Calculator.add(33, 9));
+        System.out.println("75 - 33 = " + Calculator.subtract(75, 33));
+        System.out.println("6 * 7 = " + Calculator.multiply(6, 7));
+        System.out.println("420 / 10 = " + Calculator.divide(420, 10));
     }
 }
