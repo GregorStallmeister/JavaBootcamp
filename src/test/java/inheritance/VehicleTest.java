cf536aea5d80a0a624f749d1a4bc3c96c1b0252a
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-05 11:10:14 +0100

    implement tests equalsExpectedTrueWhenCompareToCarsIdenticallyInstantiated and equalsExpectedFalsWhenCompareToCarsIdenticallyButYearInstantiated

--- src/test/java/inheritance/VehicleTest.java
+++ src/test/java/inheritance/VehicleTest.java
@@ -1,4 +1,36 @@
 package inheritance;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VehicleTest {
+
+    @Test
+    public void equalsExpectedTrueWhenCompareToCarsIdenticallyInstantiated() {
+        // given
+        Vehicle veh1 = new Vehicle("Opel", "Meriva A", 2009);
+        Vehicle veh2 = new Vehicle("Opel", "Meriva A", 2009);
+        boolean expected = true;
+
+        // when
+        boolean actual = veh1.equals(veh2);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void equalsExpectedFalsWhenCompareToCarsIdenticallyButYearInstantiated() {
+        // given
+        Vehicle veh1 = new Vehicle("Opel", "Meriva A", 2009);
+        Vehicle veh2 = new Vehicle("Opel", "Meriva A", 2007);
+        boolean expected = false;
+
+        // when
+        boolean actual = veh1.equals(veh2);
+
+        // then
+        assertEquals(expected, actual);
+    }
 }
