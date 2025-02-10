3cda4d1cff1fdb6bae578f09524f21655a710500
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-10 11:59:02 +0100

    implement methods Account.depositMoney and Account.withdrawMoney

--- src/main/java/bigDecimal/Account.java
+++ src/main/java/bigDecimal/Account.java
@@ -6,4 +6,12 @@ public class Account {
     String accountNumber;
     BigDecimal accountBalance;
     Client customer;
+
+    public void depositMoney (BigDecimal deposit) {
+        accountBalance.add(deposit);
+    }
+
+    public void withdrawMoney(BigDecimal withdrawal) {
+        accountBalance.subtract(withdrawal);
+    }
 }
